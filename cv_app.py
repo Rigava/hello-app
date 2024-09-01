9d362f15cee010c5d5a45a4150c7f27bf3bbcf13
NetZero <73232007+Rigava@users.noreply.github.com>
2024-09-01 15:51:53 +0530

    Update cv_app.py

--- cv_app.py
+++ cv_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-
+st.set_page_config(page_title="Decarb sailor", page_icon=":bar_chart:", layout="wide")
 
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
