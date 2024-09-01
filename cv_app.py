9f4bf2ed81d8a740bc2534bd119699e814402a8b
NetZero <73232007+Rigava@users.noreply.github.com>
2024-09-01 15:49:05 +0530

    Update cv_app.py
    1c

--- cv_app.py
+++ cv_app.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
-
+st.set_page_config(page_title="Decarb sailor", page_icon=":bar_chart:", layout="wide")
 #####################
 # Header 
 st.write('''
