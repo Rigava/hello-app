45213a423deadf4f6632bd06117c0faed84e1a50
NetZero <73232007+Rigava@users.noreply.github.com>
2024-09-01 11:03:14 +0530

    Update cv_app.py
    1c

--- cv_app.py
+++ cv_app.py
@@ -36,7 +36,7 @@ st.markdown("""
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link disabled" href="https://akj786.pythonanywhere.com/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#education">Education</a>
